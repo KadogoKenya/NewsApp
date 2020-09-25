233347abd979bea13cf304e2f2694b13106a9729
Kate <you@example.com>
2020-09-25 20:18:29 +0300

    desription details added in models

--- app/models.py
+++ app/models.py
@@ -20,7 +20,7 @@ class Sources:
     def __init__(self,id,name,description,url,category,language,country):
         self.id = id
         self.name = name
-        # self.description = description
+        self.description = description
         self.url = url
         self.category = category
         self.language = language
